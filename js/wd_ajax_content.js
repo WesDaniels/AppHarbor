5823033ecd6d1cc1d877ade065415f3b3701930e
github@wesdaniels.com <github@wesdaniels.com>
2013-01-05 23:38:48 -0600

    Newest Version of the website, now using AJAX and a REST service to build pages
    Signed-off-by: github@wesdaniels.com <github@wesdaniels.com>

---
+++ js/wd_ajax_content.js
@@ -0,0 +1,10 @@
+﻿$('.siteArticle').click(function () {
+    $.ajax({
+        url: 'dataservices/SiteArticles.svc/load/' + this.id,
+        success: function (data) {
+            $.each(data, function (i, item) {
+                $('#contentText').html(item)
+            });
+        }
+    });
+});
