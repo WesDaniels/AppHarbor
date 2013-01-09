e743fdd12baa632e806da7a30ebdbbd37720e5c0
github@wesdaniels.com <github@wesdaniels.com>
2013-01-08 19:05:07 -0600

    Clean Deploy Round 3
    Signed-off-by: github@wesdaniels.com <github@wesdaniels.com>

---
+++ js/wd_ajax_pastebin.js
@@ -0,0 +1,21 @@
+﻿function getPastebin(id) {
+    var start = new Date()
+    $('#' + id).after('<div class=\'api-talk\' >API: Retrive http://pastebin.com/raw.php?i=' + id + '</div>');
+    $.ajax({
+        url: 'dataservices/pastebin/pastebinproxy.svc/' + id,
+        success: function (data) {
+            $.each(data, function (i, item) {
+                $('#' + id).html(htmlEncode(item));
+            });
+            prettyPrint();
+        }
+    });
+}
+
+function htmlEncode(value) {
+    if (value) {
+        return jQuery('<div />').text(value).html();
+    } else {
+        return '';
+    }
+}
